e70b47527458a1b8ec487d058a764b86b8481e56
ruserxd <zxcmomo0219@gmail.com>
2024-08-03 09:11:00 +0800

    make comment for getCommitTimestamp

--- src/main/java/com/example/codemangesystem/GitCloner.java
+++ src/main/java/com/example/codemangesystem/GitCloner.java
@@ -22,6 +22,7 @@ public class GitCloner {
     private static final Logger logger = LoggerFactory.getLogger(GitCloner.class);
     private static final String DEFAULT_BRANCH = "main";
 
+    // 將儲存庫複製到本地資料夾，並按照commit時間做分類，並回傳最終的路徑
     // 可能丟出 GitAPIException(Git操作錯誤) 和 IOException(檔案操作錯誤)
     public String cloneRepository(String repoUrl) throws GitAPIException, IOException {
         String repoName = getRepoNameFromUrl(repoUrl);
@@ -47,6 +48,7 @@ public class GitCloner {
         }
     }
 
+    // 從儲存庫網址中取得專案名稱
     private String getRepoNameFromUrl(String repoUrl) {
         // 將網址透過"/"分開
         String[] parts = repoUrl.split("/");
@@ -56,15 +58,21 @@ public class GitCloner {
         return repoNameWithExtension.replace(".git", "");
     }
 
-    private String getCommitTimestamp(String localPath) throws IOException, GitAPIException {
+    // 取得最新提交的時間戳
+    private String getCommitTimestamp(String localPath) throws IOException {
+        // 先將本地儲存庫打開 -> 取得儲存庫物件 -> 取得HEAD的commit ID -> 透過RevWalk取得RevCommit -> 轉換成Instant -> 格式化時間
         try (Git git = Git.open(new File(localPath))) {
             Repository repository = git.getRepository();
+            // 取得HEAD的commit ID
             ObjectId headCommitId = repository.resolve(DEFAULT_BRANCH);
             try (RevWalk revWalk = new RevWalk(repository)) {
+                //RevCommit 包含了提交的作者、訊息、時間戳等資訊
                 RevCommit commit = revWalk.parseCommit(headCommitId);
+                // Git 儲存庫中的提交時間戳是 UTC 時間 (從 1970-01-01T00:00:00Z 開始的秒數)，避免掉不同時區的問題
+                // 取得commit時間並轉換成Instant(UTC時間)
                 Instant commitTime = Instant.ofEpochSecond(commit.getCommitTime());
-                return commitTime.atZone(ZoneId.systemDefault())
-                        .format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
+                //將Instant轉換成Zone系統預設時區的時間，並格式化成yyyyMMdd_HHmmss字串
+                return commitTime.atZone(ZoneId.systemDefault()).format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
             }
         }
     }
