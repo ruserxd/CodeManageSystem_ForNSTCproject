c5a205b4aed84b6848e061709bef8c4cbbe09a9b
ruserxd <zxcmomo0219@gmail.com>
2024-12-04 21:24:38 +0800

    Add UserServiceTest not finish

---
+++ backend/src/test/java/com/codemangesystem/loginProcess/service/UserServiceTest.java
@@ -0,0 +1,22 @@
+package com.codemangesystem.loginProcess.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = UserServiceTest.class)
+class UserServiceTest {
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void test() {
+
+    }
+}
